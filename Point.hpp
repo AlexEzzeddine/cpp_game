9f541631fc78a98c5df68bbfbf03c3456d15ed78
alex <alex.izzedin@gmail.com>
2018-01-13 15:15:58 -0800

    point declaration

---
+++ Point.hpp
@@ -0,0 +1,14 @@
+class Point
+{
+	private:
+		int x;
+		int y;
+	public:
+		Point();
+		Point(Point &point);
+		Point(int x, int y);
+		int getX();
+		int getY();
+		void setX(int x);
+		void setY(int y);
+}
