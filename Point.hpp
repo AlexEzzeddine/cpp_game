d0a89a35162e00e99f875623ffe02df786dae86a
alex <alex.izzedin@gmail.com>
2018-01-13 19:30:02 -0800

    Point class is fully functional now

--- Point.hpp
+++ Point.hpp
@@ -1,3 +1,6 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
 class Point
 {
 	private:
@@ -7,10 +10,16 @@ class Point
 		Point();
 		Point(Point &point);
 		Point(int x, int y);
+		~Point();
+		
 		int getX();
 		int getY();
 		void setX(int x);
 		void setY(int y);
+		void setCoords(int x, int y);
 
+		Point& operator=(Point const& point);
 		bool operator==(Point const& point) const;
 }
+
+#endif
