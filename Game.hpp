a26f6a9e9baae856d13e282ef3673ad336a3feaf
Tim Jose <timjose3@gmail.com>
2018-01-13 19:01:20 -0800

    update

--- Game.hpp
+++ Game.hpp
@@ -6,9 +6,6 @@
 #include <string>
 #define FRAMES_PER_SECOND 60
 #define CLOCKS_PER_FRAME (CLOCKS_PER_SEC / FRAMES_PER_SECOND)
-#define WIN_HEI 40
-#define WIN_WID 80
-#define RIGHT_WIN_WID 65
 #define MAX_ENEMIES 10
 
 class Game
@@ -16,6 +13,8 @@ class Game
     private:
         WINDOW*     _win;
         bool        _finished;
+        int         _maxHei;
+        int         _maxWid;
         // Player*      _p1;
         // Enemy        _enemies[MAX_ENEMIES];
 
@@ -25,7 +24,6 @@ class Game
         ~Game();
         // Game & operator=(Game const & rhs);
         void    initScreen();
-        void    redraw();
         void    handleInput(char c);
         void    drawEntities();
         void    moveEntities();
--- main.cpp
+++ main.cpp
@@ -1,24 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aezzeddi <aezzeddi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/13 14:36:55 by aezzeddi          #+#    #+#             */
-/*   Updated: 2018/01/13 14:54:27 by aezzeddi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "Game.hpp"
 
-#include <ncurses.h>
+int main() 
+{
+    Game* g = new Game();    
 
-int main()
-{	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
 
-	return 0;
+    delete g;    
+    return 0;
 }
